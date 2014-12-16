de718cdc1d19ace765afd6cb4643d861ec01476c
Jari K <jariak@hotmail.com>
2014-12-16 15:46:07 +0200

    Dataset fetching etc. added

--- run_analysis.R
+++ run_analysis.R
@@ -1,27 +1,50 @@
+# Coursera: Getting and Cleaning Data, Course Project
+#
+# run_analysis.R
+
 library(reshape2)
 library(data.table)
 
-# To standardize field separating space and remove leading blanks from the X-files
-# before reading in with fread(). Sed is installed by default in all Linux/Mac OSs.
-# Could use standard read.table() instead, but fread() is about tenfold faster,
-# so it is worth some effort to make it work as smooth as possible. This piece of
-# preprocessing made fread() more stable on my Linux system. The data.table package
-# is still quite fresh, and this will hopefully become unnecessary in the future.
+# Fetch and unzip dataset if not exists in the working directory
+if (!file.exists("UCI HAR Dataset")) {
+    fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+    tmp.file <- tempfile()
+    download.file(fileURL, destfile=tmp.file, method="curl")
+    #download(fileURL, tmp.file, mode = "wb") # with 'downloader' package
+    unzip(tmp.file)
+    unlink(tmp.file)
+}
+
+# sed command to standardize field separating space and to remove leading blanks
+# from the large test and training data files (X_test.txt and X_train.txt) 
+# before reading in with fread(). sed is a common utility installed by default 
+# on all Linux/Mac OS's, and available for free on every platform that has a 
+# version of R. fread() is about tenfold faster than standard read.table(), so
+# it's worth some effort to make it work as smooth as possible. This piece of
+# preprocessing should be unnecessary, but it made fread() more stable on my
+# Linux system when reading those two large data files.
+#
+# However, in the final version of this script I commented out the parts that
+# used sed, and replaced them with standard read.table() calls to remove any
+# reliance on software outside R packages, to make it easy to verify the script
+# on any platform.
 sedcmd <- "sed -e 's/  */ /g' -e 's/^ //'"
 
 # Read in test set data
-X_test <- fread(paste(sedcmd, "UCI_HAR_Dataset/test/X_test.txt"))
-y_test <- fread("UCI_HAR_Dataset/test/y_test.txt")[[1]]
-subject_test <- fread("UCI_HAR_Dataset/test/subject_test.txt")[[1]]
+X_test <- fread(paste(sedcmd, "'UCI HAR Dataset/test/X_test.txt'"))      # sed
+#X_test <- as.data.table(read.table("UCI HAR Dataset/test/X_test.txt"))    # standard (slow)
+y_test <- fread("UCI HAR Dataset/test/y_test.txt")[[1]]
+subject_test <- fread("UCI HAR Dataset/test/subject_test.txt")[[1]]
 
 # Read in training set data
-X_train <- fread(paste(sedcmd, "UCI_HAR_Dataset/train/X_train.txt"))
-y_train <- fread("UCI_HAR_Dataset/train/y_train.txt")[[1]]
-subject_train <- fread("UCI_HAR_Dataset/train/subject_train.txt")[[1]]
+X_train <- fread(paste(sedcmd, "'UCI HAR Dataset/train/X_train.txt'"))   # sed
+#X_train <- as.data.table(read.table("UCI HAR Dataset/train/X_train.txt")) # standard (slow)
+y_train <- fread("UCI HAR Dataset/train/y_train.txt")[[1]]
+subject_train <- fread("UCI HAR Dataset/train/subject_train.txt")[[1]]
 
 # Read in the activity labels and features
-activity_labels <- fread("UCI_HAR_Dataset/activity_labels.txt", stringsAsFactors=FALSE)
-features <- fread("UCI_HAR_Dataset/features.txt", stringsAsFactors=FALSE)[[2]]
+activity_labels <- fread("UCI HAR Dataset/activity_labels.txt", stringsAsFactors=FALSE)
+features <- fread("UCI HAR Dataset/features.txt", stringsAsFactors=FALSE)[[2]]
 
 # Find all features that include 'mean()' or 'std()' in their names
 meansd_cols <- grep("(mean|std)\\(\\)", features, ignore.case=TRUE)
@@ -39,19 +62,20 @@ setnames(X_train, features)
 x_meansd <- rbind(X_test, X_train)[, meansd_cols, with=FALSE]
 
 # Combine testing and training subject and activity columns and add them to the X data as key
-subject.id <- c(subject_test, subject_train)
+subject <- c(subject_test, subject_train)
 activity <- c(y_test, y_train)
-x_meansd <- cbind(subject.id, activity, x_meansd)
-setkey(x_meansd, subject.id, activity)
+x_meansd <- cbind(subject, activity, x_meansd)
+setkey(x_meansd, subject, activity)
 
 # Count the means of each feature per subject and activity
-res <- x_meansd[, lapply(.SD, mean), by=.(subject.id, activity)]
+res <- x_meansd[, lapply(.SD, mean), by=.(subject, activity)]
 
 # Replace activity codes with labels
 res[, activity := factor(activity, labels=activity_labels[[2]])]
 
 # Convert to long form
-m <- melt(res, id.vars=c("subject.id","activity"), variable.name="feature", value.name="mean")
+m <- melt(res, id.vars=c("subject","activity"), variable.name="feature", value.name="mean")
 
-# Write tidy data
+# Write tidy data in a file
+# (Read back in with: read.table("m.txt", header=TRUE)
 #write.table(m, "m.txt", row.names=FALSE, quote=FALSE)
